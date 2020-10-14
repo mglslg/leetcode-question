cbd1fef8faef62b31a4ed09a895b190f7338917e
索龙嘎 <suolongga@huli.com>
2020-10-14 18:23:42 +0800

    123

--- src/main/java/com/slg/leetcode/editor/cn/IntersectionOfTwoLinkedLists.java
+++ src/main/java/com/slg/leetcode/editor/cn/IntersectionOfTwoLinkedLists.java
@@ -1,63 +1,43 @@
 package com.slg.leetcode.editor.cn;
 
 import com.slg.leetcode.editor.cn.ds.ListNode;
-import com.slg.leetcode.editor.cn.util.UseCaseUtil;
 
 public class IntersectionOfTwoLinkedLists{
         public static void main(String[] args) {
-            UseCaseUtil.buildLinkedList("[11,12,13,14,15,16,17,18,19,20,21,22,23,24]");
             Solution solution = new IntersectionOfTwoLinkedLists().new Solution();
         }
 
         //leetcode submit region begin(Prohibit modification and deletion)
-    /**
-     * 思路：首先判断两条链表的长度,长的那一条先遍历到与短的那条齐头并进时,此时同时遍历看是否能相交
-     */
-    public class Solution {
-        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-            int lengthA = getLength(headA);
-            int lengthB = getLength(headB);
-            int delta = Math.abs(lengthA - lengthB);
-            if (lengthA > lengthB) {
-                ListNode pA = headA;
-                for (int i = 0; i < delta; i++) {
-                    pA = pA.next;
+        /**
+         * 思路：p1,p2同时遍历两条链表，到尾之后链到另一个头上
+         * 就等于每个指针都完整遍历一次两条链表
+         * 每个链表被遍历两次,p1p2最终走过的路程相等,并在终点相遇
+         * "可以理解成两个人速度一致， 走过的路程一致。那么肯定会同一个时间点到达终点。如果到达终点的最后一段路两人都走的话，那么这段路上俩人肯定是肩并肩手牵手的"
+         */
+        public class Solution {
+            public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+                ListNode pa = headA;
+                ListNode pb = headB;
+                boolean joinedA = false;
+                boolean joinedB = false;
+                while(pa.next!=null || !joinedA){
+                    if (pa.next == null && !joinedA) {
+                        pa.next = headB;
+                        joinedA = true;
+                    }
+                    if (pb.next == null && !joinedB) {
+                        pb.next = headA;
+                        joinedB = true;
+                    }
+                    if(pa==pb){
+                        return pa;
+                    }else{
+                        pa = pa.next;
+                        pb = pb.next;
+                    }
                 }
-                return getIntersection(pA, headB);
+                return null;
             }
-            if(lengthA < lengthB){
-                ListNode pB = headB;
-                for (int i = 0; i < delta; i++) {
-                    pB = pB.next;
-                }
-                return getIntersection(headA, pB);
-            }
-            if (lengthA == lengthB) {
-                return getIntersection(headA, headB);
-            }
-            return null;
         }
-
-        private ListNode getIntersection(ListNode p1, ListNode p2) {
-            while (p1 != null && p1 != p2) {
-                p1 = p1.next;
-                p2 = p2.next;
-            }
-            return p1;
-        }
-
-        private int getLength(ListNode n){
-            ListNode p = n;
-            if (p == null) {
-                return 0;
-            }
-            int length=1;
-            while (p.next != null) {
-                length++;
-                p = p.next;
-            }
-            return length;
-        }
-    }
         //leetcode submit region end(Prohibit modification and deletion)
 }
