f43152621273e1539d3e8d561e0b72bfe1ded902
索龙嘎 <suolongga@huli.com>
2020-10-19 13:46:24 +0800

    123

--- src/main/java/com/slg/leetcode/editor/cn/LinkedListCycle.java
+++ src/main/java/com/slg/leetcode/editor/cn/LinkedListCycle.java
@@ -1,29 +1,37 @@
 package com.slg.leetcode.editor.cn;
 
 import com.slg.leetcode.editor.cn.ds.ListNode;
+import com.slg.leetcode.editor.cn.util.UseCaseUtil;
 
 public class LinkedListCycle{
         public static void main(String[] args) {
-                Solution solution = new LinkedListCycle().new Solution();
+            Solution solution = new LinkedListCycle().new Solution();
+            ListNode head = UseCaseUtil.buildLinkedList("[1,2]");
+            System.out.println(solution.hasCycle(head));
         }
 
         //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        return false;
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+        public class Solution {
+            public boolean hasCycle(ListNode head) {
+                ListNode fast = head;
+                ListNode slow = head;
+
+                if (head==null) {
+                    return false;
+                }
+
+                while (slow != null) {
+                    slow = slow.next;
+                    if (fast != null) {
+                        fast = fast.next != null ? fast.next.next : null;
+                    }
+                    if (slow == fast && slow != null) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        //leetcode submit region end(Prohibit modification and deletion)
 	
 }
