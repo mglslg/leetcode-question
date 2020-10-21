1fe2b769e5796cd0ab64bb169fa875c2055e898e
suolongga <mglslg@qq.com>
2020-10-21 23:54:29 +0800

    123

--- src/main/java/com/slg/leetcode/editor/cn/RotateList.java
+++ src/main/java/com/slg/leetcode/editor/cn/RotateList.java
@@ -8,15 +8,12 @@ public class RotateList{
                 Solution solution = new RotateList().new Solution();
         }
 
-        //leetcode submit region begin(Prohibit modification and deletion)
         /**
          * 第一种解法：k次旋转，每一次旋转都是把最后一个节点拿上然后拼到头上
          * 构建一个stack，从里面取出最后一个节点和倒数第二个节点拼头上
          * 同时需要注意k > size 的问题，取模即可
-         *
-         * 第二种解法：将k取模拿到最小k,之后将最后k个元素拼头上，快慢指针搞定即可
          */
-        class Solution {
+        class Solution1 {
             public ListNode rotateRight(ListNode head, int k) {
                 if (head == null) {
                     return null;
@@ -40,6 +37,40 @@ public class RotateList{
                 return currHead;
             }
         }
+
+        //leetcode submit region begin(Prohibit modification and deletion)
+        /**
+         * 第二种解法：将k取模拿到最小k,之后将最后k个元素拼头上，快慢指针搞定即可
+         */
+        class Solution {
+                public ListNode rotateRight(ListNode head, int k) {
+                    if (head == null) {
+                        return head;
+                    }
+                    ListNode slow = head;
+                    ListNode fast = head;
+                    for (int i = 0; i < k; i++) {
+                        if (fast.next == null) {
+                            //超过一轮时重新开始走,这边也可以优化成模(但是前提需要知道链表总长度至少要遍历一次),可以免去不停的循环遍历
+                            fast = head;
+                        }else{
+                            fast = fast.next;
+                        }
+                    }
+                    //快慢指针间距离为0，移动0个位置，直接返回head
+                    if (fast == head) {
+                        return head;
+                    }
+                    while (fast.next != null) {
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+                    ListNode newHead = slow.next;
+                    fast.next = head;
+                    slow.next = null;
+                    return newHead;
+                }
+            }
         //leetcode submit region end(Prohibit modification and deletion)
 	
 }
