3e2fc1e372e64077fed56141927ad7fba94501e7
索龙嘎 <suolongga@huli.com>
2020-10-30 17:58:19 +0800

    123

---
+++ src/main/java/com/slg/leetcode/editor/cn/ImplementStrstr.java
@@ -0,0 +1,34 @@
+package com.slg.leetcode.editor.cn;
+
+public class ImplementStrstr{
+        public static void main(String[] args) {
+                Solution solution = new ImplementStrstr().new Solution();
+        }
+
+        //leetcode submit region begin(Prohibit modification and deletion)
+        class Solution {
+            public int strStr(String haystack, String needle) {
+                char[] hayChars = haystack.toCharArray();
+                char[] needleChars = needle.toCharArray();
+                int idx = 0;
+                int startIdx = 0;
+                for (int i = 0; i < hayChars.length; i++) {
+                    if (idx < needleChars.length) {
+                        if (hayChars[i] == needleChars[idx]) {
+                            if (startIdx == 0) {
+                                startIdx = i;
+                            }
+                            idx++;
+                        }else{
+                            startIdx = -1;
+                        }
+                    }else{
+                        return startIdx;
+                    }
+                }
+                return -1;
+            }
+        }
+        //leetcode submit region end(Prohibit modification and deletion)
+	
+}
