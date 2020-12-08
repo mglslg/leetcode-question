50b67211fc73258743b3848c36ed0781e4b67c15
索龙嘎 <suolongga@huli.com>
2020-12-08 17:53:37 +0800

    Merge branch 'master' of github.com:mglslg/leetcode-question
    # Conflicts:
    #	src/main/java/com/slg/leetcode/editor/cn/all.json
    #	src/main/java/com/slg/leetcode/editor/cn/translation.json

---
+++ src/main/java/com/slg/leetcode/editor/cn/LongestValidParentheses.java
@@ -0,0 +1,16 @@
+package com.slg.leetcode.editor.cn;
+
+public class LongestValidParentheses{
+        public static void main(String[] args) {
+                Solution solution = new LongestValidParentheses().new Solution();
+        }
+
+        //leetcode submit region begin(Prohibit modification and deletion)
+        class Solution {
+            public int longestValidParentheses(String s) {
+                return 0;
+            }
+        }
+        //leetcode submit region end(Prohibit modification and deletion)
+	
+}
