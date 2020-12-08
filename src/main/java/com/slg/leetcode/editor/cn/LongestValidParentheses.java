8814c86f4078d147405e1e9ce56498ada38695e8
suolongga <mglslg@qq.com>
2020-12-09 00:17:59 +0800

    123

--- src/main/java/com/slg/leetcode/editor/cn/LongestValidParentheses.java
+++ src/main/java/com/slg/leetcode/editor/cn/LongestValidParentheses.java
@@ -1,16 +1,36 @@
 package com.slg.leetcode.editor.cn;
 
-public class LongestValidParentheses{
-        public static void main(String[] args) {
-                Solution solution = new LongestValidParentheses().new Solution();
-        }
+import java.util.Stack;
 
-        //leetcode submit region begin(Prohibit modification and deletion)
-        class Solution {
-            public int longestValidParentheses(String s) {
-                return 0;
+public class LongestValidParentheses {
+    public static void main(String[] args) {
+        Solution solution = new LongestValidParentheses().new Solution();
+        solution.longestValidParentheses(")))()())(()");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    public class Solution {
+        public int longestValidParentheses(String s) {
+            int maxans = 0;
+            Stack<Integer> stack = new Stack();
+            //栈底永远是每一组开头的前一个值的索引!!!
+            stack.push(-1);
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    stack.push(i);
+                } else {
+                    //只要遇到右括号就弹，弹完最后栈底剩下的那个不为空的值就是下一组的开始索引-1
+                    stack.pop();
+                    if (stack.empty()) {
+                        stack.push(i);
+                    } else {
+                        maxans = Math.max(maxans, i - stack.peek());
+                    }
+                }
             }
+            return maxans;
         }
-        //leetcode submit region end(Prohibit modification and deletion)
-	
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
 }
