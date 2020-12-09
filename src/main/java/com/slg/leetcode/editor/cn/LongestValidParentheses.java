0928890e75bd7dee6304afc2a81e623782fe7a97
suolongga <mglslg@qq.com>
2020-12-09 22:51:15 +0800

    123

--- src/main/java/com/slg/leetcode/editor/cn/LongestValidParentheses.java
+++ src/main/java/com/slg/leetcode/editor/cn/LongestValidParentheses.java
@@ -10,13 +10,28 @@ public class LongestValidParentheses {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     public class Solution {
+        /**
+         * 这个解答不行！解决不了 (() 的情况
+         */
         public int longestValidParentheses(String s) {
             int max = 0;
-            int idx = 0;
+            int startIdx = 0;
             Stack<Integer> stack = new Stack();
-
-
-
+            for (int i = 0; i < s.length(); i++) {
+                if(s.charAt(i) == '('){
+                    stack.push(i);
+                }else{
+                    if (!stack.empty() && stack.peek() == '(') {
+                        stack.pop();
+                        int length = i - startIdx + 1;
+                        if (length > max) {
+                            max = length;
+                        }
+                    } else {
+                        startIdx = i;
+                    }
+                }
+            }
             return max;
         }
     }
