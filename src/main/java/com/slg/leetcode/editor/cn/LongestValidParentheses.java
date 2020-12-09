a09bf5a89d90b0e11380b656cd48c1cd8acead2e
索龙嘎 <suolongga@huli.com>
2020-12-09 18:39:48 +0800

    Merge branch 'master' of github.com:mglslg/leetcode-question
    # Conflicts:
    #	src/main/java/com/slg/leetcode/editor/cn/all.json
    #	src/main/java/com/slg/leetcode/editor/cn/translation.json

--- src/main/java/com/slg/leetcode/editor/cn/LongestValidParentheses.java
+++ src/main/java/com/slg/leetcode/editor/cn/LongestValidParentheses.java
@@ -11,24 +11,13 @@ public class LongestValidParentheses {
     //leetcode submit region begin(Prohibit modification and deletion)
     public class Solution {
         public int longestValidParentheses(String s) {
-            int maxans = 0;
+            int max = 0;
+            int idx = 0;
             Stack<Integer> stack = new Stack();
-            //栈底永远是每一组开头的前一个值的索引!!!
-            stack.push(-1);
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '(') {
-                    stack.push(i);
-                } else {
-                    //只要遇到右括号就弹，弹完最后栈底剩下的那个不为空的值就是下一组的开始索引-1
-                    stack.pop();
-                    if (stack.empty()) {
-                        stack.push(i);
-                    } else {
-                        maxans = Math.max(maxans, i - stack.peek());
-                    }
-                }
-            }
-            return maxans;
+
+
+
+            return max;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
