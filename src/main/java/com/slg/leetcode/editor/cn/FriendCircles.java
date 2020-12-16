18570ec4c0d5d91ffb2e537c989c32b45128c81d
suolongga <mglslg@qq.com>
2020-12-17 00:17:50 +0800

    123

---
+++ src/main/java/com/slg/leetcode/editor/cn/FriendCircles.java
@@ -0,0 +1,54 @@
+package com.slg.leetcode.editor.cn;
+
+public class FriendCircles{
+        public static void main(String[] args) {
+                Solution solution = new FriendCircles().new Solution();
+        }
+
+        //leetcode submit region begin(Prohibit modification and deletion)
+        class Solution {
+            private int[] parents = null;
+            private int count = 0;
+            public int findCircleNum(int[][] M) {
+                count = M.length;
+                parents = makeSet(M.length);
+                for (int i = 0; i < M.length; i++) {
+                    for (int j = 0; j < M[i].length; j++) {
+                        if (M[i][j] == 1) {
+                            //如果两人有边，尝试合并
+                            union(i,j);
+                        }
+                    }
+                }
+                return count;
+            }
+
+            //初始化将每一个节点的父节点指为自己
+            private int[] makeSet(int length){
+                parents = new int[length];
+                for (int i = 0; i < length; i++) {
+                    parents[i] = i;
+                }
+                return parents;
+            }
+
+            private int findRoot(int node) {
+                if (node != parents[node]) {
+                    return parents[node] = findRoot(parents[node]);
+                    //return findRoot(parents[node]); //可是why?????
+                }else {
+                    return node;
+                }
+            }
+
+            private void union(int x, int y) {
+                if (findRoot(x) == findRoot(y)) {
+                    return ;
+                }
+                parents[parents[x]] = parents[y];
+                count--;
+            }
+        }
+        //leetcode submit region end(Prohibit modification and deletion)
+	
+}
