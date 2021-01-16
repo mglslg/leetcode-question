fe82cda8f9a7b31b10edc19c70d3be2741ced07f
suolongga <mglslg@qq.com>
2021-01-16 22:56:47 +0800

    123

--- src/main/java/com/slg/leetcode/editor/cn/MergeTwoSortedLists.java
+++ src/main/java/com/slg/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -9,7 +9,39 @@ public class MergeTwoSortedLists{
 
         //leetcode submit region begin(Prohibit modification and deletion)
         class Solution {
+            /**
+             * 递归思路：
+             * 1、首先确定递归函数返回的是啥，一定是返回l1和l2中间更小的那个
+             * 2、确定了递归的返回值，接下来就要考虑如何在递归逻辑中把返回值给运用起来
+             * 3、如果返回的是l1，那就需要让l1后面拼接上l1.next和l2的拼接，这就递归起来了
+             */
+            public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+                if (l1 == null) {
+                    return l2;
+                }
+                if (l2 == null) {
+                    return l1;
+                }
+                if (l1.val < l2.val) {
+                    if (l1.next != null) {
+                        l1.next = mergeTwoLists(l1.next, l2);
+                    }else{
+                        l1.next = l2;
+                    }
+                    return l1;
+                }else{
+                    if (l2.next != null) {
+                        l2.next = mergeTwoLists(l2.next, l1);
+                    }else{
+                        l2.next = l1;
+                    }
+                    return l2;
+                }
+            }
+        }
+        //leetcode submit region end(Prohibit modification and deletion)
 
+        class Solution1 {
             /**
              * 思路：遍历l1,当p1大于等于p2时将p2节点链到p1前面，否则p1向后移动
              */
@@ -55,6 +87,4 @@ public class MergeTwoSortedLists{
                 return l1;
             }
         }
-        //leetcode submit region end(Prohibit modification and deletion)
-	
 }
