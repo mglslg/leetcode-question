d44ba4df95bc1bcffa05ad727c5230530ab07933
索龙嘎 <suolongga@huli.com>
2021-01-29 17:55:06 +0800

    Merge branch 'master' of github.com:mglslg/leetcode-question
    # Conflicts:
    #	src/main/java/com/slg/leetcode/editor/cn/all.json

--- src/main/java/com/slg/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/com/slg/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -5,6 +5,9 @@ import com.slg.leetcode.editor.cn.ds.TreeNode;
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
     public static void main(String[] args) {
         Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
+        int[] preorder = new int[]{3, 9, 20, 15, 7};
+        int[] inorder = new int[]{9, 3, 15, 20, 7};
+        TreeNode root = solution.buildTree(preorder, inorder);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -21,19 +24,19 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
         }
 
         private TreeNode getTree(int currRootIdx, int startIdx, int endIdx) {
-            int currVal=preorderList[currRootIdx];
+            int rootVal=preorderList[currRootIdx];
             TreeNode root = new TreeNode();
-            root.val = currVal;
+            root.val = rootVal;
 
             for (int i = startIdx; i <= endIdx; i++) {
-                if (inorderList[i] == currVal) {
+                if (inorderList[i] == rootVal) {
                     if (i - 1 >= startIdx) {
-                        root.left = getTree(currRootIdx++, startIdx, i - 1);
+                        root.left = getTree(currRootIdx+1, startIdx, i - 1);
                     }else{
                         //已经向左走到头了，该右边了
                         if (endIdx >= i + 1) {
                             //右侧至少还剩一个，连到右孩子上
-                            root.right = getTree(currRootIdx++, i + 1, endIdx);
+                            root.right = getTree(currRootIdx+1, i + 1, endIdx);
                         }
                     }
                 }
