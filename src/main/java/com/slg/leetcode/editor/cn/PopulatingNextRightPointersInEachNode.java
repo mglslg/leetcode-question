782e1b4b36163443e6a6832810d1f8cf961a4fff
索龙嘎 <suolongga@huli.com>
2021-03-01 17:03:34 +0800

    Merge branch 'master' of github.com:mglslg/leetcode-question
    # Conflicts:
    #	src/main/java/com/slg/leetcode/editor/cn/all.json

--- src/main/java/com/slg/leetcode/editor/cn/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/com/slg/leetcode/editor/cn/PopulatingNextRightPointersInEachNode.java
@@ -10,14 +10,42 @@ public class PopulatingNextRightPointersInEachNode {
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
+    /**
+     * 思路：广度遍历加上一个记录层级的变量,行末加入null,然后依次connect即可
+     */
     class Solution {
         public Node connect(Node root) {
-            Node p = root;
+            if (root == null) {
+                return null;
+            }
+
             LinkedList<Node> queue = new LinkedList<>();
             queue.add(root);
+            queue.add(null);
+            int level = 0;
+            int count = 1;
 
-            //todo 哎嘿，挺有意思的啊！
-
+            while (!queue.isEmpty()) {
+                if (count == 0) {
+                    //到了行末,进入下一行
+                    queue.add(null);
+                    level++;
+                    count = (int) Math.pow(2, level);
+                } else {
+                    Node curr = queue.pollFirst();
+                    if (curr == null) {
+                        //上一行的最后一个null值,直接跳过
+                        continue;
+                    }
+                    curr.next = queue.peekFirst();
+                    //不为叶子节点
+                    if (curr.left != null) {
+                        queue.add(curr.left);
+                        queue.add(curr.right);
+                    }
+                    count--;
+                }
+            }
             return root;
         }
     }
