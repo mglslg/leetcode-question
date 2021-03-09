28164cf4b1670929aa741e9e71b2b260aafc5329
suolongga <mglslg@qq.com>
2021-03-09 22:25:47 +0800

    Merge branch 'master' of github.com:mglslg/leetcode-question
    # Conflicts:
    #	src/main/java/com/slg/leetcode/editor/cn/all.json
    #	src/main/java/com/slg/leetcode/editor/cn/translation.json

--- src/main/java/com/slg/leetcode/editor/cn/PopulatingNextRightPointersInEachNodeIi.java
+++ src/main/java/com/slg/leetcode/editor/cn/PopulatingNextRightPointersInEachNodeIi.java
@@ -1,39 +1,35 @@
 package com.slg.leetcode.editor.cn;
 
-public class PopulatingNextRightPointersInEachNodeIi{
-        public static void main(String[] args) {
-                Solution solution = new PopulatingNextRightPointersInEachNodeIi().new Solution();
+import com.slg.leetcode.editor.cn.ds.Node;
+
+public class PopulatingNextRightPointersInEachNodeIi {
+    public static void main(String[] args) {
+        Solution solution = new PopulatingNextRightPointersInEachNodeIi().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        /**
+         * 这题用递归写法不好做！因为是一个广度遍历
+         */
+        public Node connect(Node root) {
+            if (root == null) {
+                return null;
+            }
+            if (root.left != null) {
+                root.left.next = root.right != null ? root.right : (root.next != null ? (root.next.left != null ? root.next.left : root.next.right) : null);
+                if (root.right != null) {
+                    root.right.next=root.next!=null?(root.next.left!=null?root.next.left:(root.next.right)):null;
+                }
+            }
+            if (root.right != null) {
+                root.right.next=root.next!=null?(root.next.left!=null?root.next.left:(root.next.right)):null;
+            }
+            connect(root.left);
+            connect(root.right);
+            return root;
         }
-
-        //leetcode submit region begin(Prohibit modification and deletion)
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
     }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
-class Solution {
-    public Node connect(Node root) {
-        
-    }
-}
 //leetcode submit region end(Prohibit modification and deletion)
-	
+
 }
