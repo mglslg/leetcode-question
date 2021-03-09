7ccc9f82cd6f4010ca482af42db07e6ca1c01819
索龙嘎 <suolongga@huli.com>
2021-03-09 15:14:54 +0800

    1

--- src/main/java/com/slg/leetcode/editor/cn/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/com/slg/leetcode/editor/cn/PopulatingNextRightPointersInEachNode.java
@@ -8,25 +8,41 @@ public class PopulatingNextRightPointersInEachNode {
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
+
     /**
      * 跳表思路，利用已经建立的root和root.next的关系来构建下一层
+     * 一个带链表的广度遍历
      */
     class Solution {
         public Node connect(Node root) {
             if (root == null) {
                 return null;
             }
-            if (root.left != null) {
-                //这个解法的问题在于只解决了左半边的问题，没有向右平移！
-                Node connectedLeft = connect(root.left);
-                Node connectedRight = connect(root.right);
-                connectedLeft.next = connectedRight;
+            //left用来记录每行最左端的那个左节点
+            Node left = root;
 
-                //哎呦！可是此时还没有root.next呢！
-                if (root.next != null) {
-                    connectedRight.next = connect(root.next).left;
+            //curr用来记录平移的那个节点
+            Node curr = null;
+
+            //仍有下一行时
+            while (left != null) {
+                curr = left;
+
+                //平移将节点链起来
+                while (curr != null && curr.left!=null) {
+                    curr.left.next = curr.right;
+                    if (curr.next != null) {
+                        curr.right.next = curr.next.left;
+                    }
+
+                    //平移到右边一个节点
+                    curr = curr.next;
                 }
+
+                //移动到下一行
+                left = left.left;
             }
+
             return root;
         }
     }
