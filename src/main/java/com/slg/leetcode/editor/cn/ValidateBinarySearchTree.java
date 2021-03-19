1f32a525af2ed6c60b6edc1d6bb607a484a953f9
索龙嘎 <suolongga@huli.com>
2021-03-19 14:37:37 +0800

    1

---
+++ src/main/java/com/slg/leetcode/editor/cn/ValidateBinarySearchTree.java
@@ -0,0 +1,19 @@
+package com.slg.leetcode.editor.cn;
+
+import com.slg.leetcode.editor.cn.ds.TreeNode;
+
+public class ValidateBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new ValidateBinarySearchTree().new Solution();
+    }
+
+    class Solution {
+        public boolean isValidBST(TreeNode root) {
+
+            return false;
+
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
