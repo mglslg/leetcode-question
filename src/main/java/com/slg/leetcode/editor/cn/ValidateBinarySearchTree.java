4f2bfd6cfca9400018349bddc156ff88f7727071
索龙嘎 <suolongga@huli.com>
2021-03-22 18:13:49 +0800

    1

--- src/main/java/com/slg/leetcode/editor/cn/ValidateBinarySearchTree.java
+++ src/main/java/com/slg/leetcode/editor/cn/ValidateBinarySearchTree.java
@@ -7,13 +7,35 @@ public class ValidateBinarySearchTree {
         Solution solution = new ValidateBinarySearchTree().new Solution();
     }
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * 思路：不能只是和自己的根节点做比较，要保证"中序遍历的升幂排列"
+     * 可以借助一个变量，每次遍历到的值都必须大于上一个遍历到的值！
+     */
     class Solution {
+        private TreeNode lastNode;
+        private boolean result = true;
+
         public boolean isValidBST(TreeNode root) {
+            if (root == null) {
+                return false;
+            }
+            inorder(root);
+            return result;
+        }
 
-            return false;
-
+        public void inorder(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            if (lastNode!=null && root.val < lastNode.val) {
+                result = false;
+            }
+            lastNode = root;
+            inorder(root.right);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
-
 }
