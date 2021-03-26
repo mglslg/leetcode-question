778b2b0d065116d47085e96d3bc33a8ae04a13df
索龙嘎 <suolongga@huli.com>
2021-03-26 14:10:28 +0800

    123

--- src/main/java/com/slg/leetcode/editor/cn/InsertIntoABinarySearchTree.java
+++ src/main/java/com/slg/leetcode/editor/cn/InsertIntoABinarySearchTree.java
@@ -10,57 +10,53 @@ public class InsertIntoABinarySearchTree {
     //leetcode submit region begin(Prohibit modification and deletion)
     /**
      * 思路：从根节点开始找，小的向左走到root.left，大的向右走到root.right
-     * 终止条件肯定是这样：根-左-右，就在这三个里面往里挤，其中讨论左和右为空的情况即可
+     * 走不动了就讨论：根-左-右，就在这三个里面往里挤，其中讨论左和右为空的情况即可
+     *
      * 也可以理解为二分查找的寻找区间
      */
     class Solution {
         public TreeNode insertIntoBST(TreeNode root, int val) {
+            if (root == null) {
+                return new TreeNode(val);
+            }
+
             TreeNode p = root;
             while (true) {
-                if (val < p.val) {
-                    //此时p是最后一个大于val的左节点
-                    p = moveLeft(p, val);
-                    if (p.left == null && p.right == null) {
-                        //已经是最小节点，直接拼到左孩子
-                        TreeNode newNode = new TreeNode(val);
+                if (p.left != null && val < p.val) {
+                    p = p.left;
+                    continue;
+                } else if (p.right != null && val > p.val) {
+                    p = p.right;
+                    continue;
+                } else {
+                    //左移和右移都已经完成了，现在val就在"根-左-右"这三个数中间，分情况讨论
+                    TreeNode newNode = new TreeNode(val);
+                    if (val < p.val) {
                         p.left = newNode;
-                        break;
-                    }else{
-                        if(){
-
+                        if (p.left != null) {
+                            //在根、左中间
+                            TreeNode left = p.left;
+                            p.left = newNode;
+                            newNode.left = left;
+                        } else {
+                            p.left = newNode;
+                        }
+                    } else {
+                        if (p.right != null) {
+                            //在根、右中间
+                            TreeNode right = p.right;
+                            p.right = newNode;
+                            newNode.right = right;
+                        } else {
+                            p.right = newNode;
                         }
                     }
-                }else{
-                    p = moveRight(p, val);
+
+                    //处理完毕退出
+                    break;
                 }
             }
             return root;
-        }
-
-        /**
-         * 返回最后一个大于val的左节点pre,val的值介于pre和pre.left之间
-         */
-        private TreeNode moveLeft(TreeNode node, int val) {
-            TreeNode pre = node;
-            TreeNode p = node.left;
-            while (p != null && val < p.val) {
-                pre = p;
-                p = p.left;
-            }
-            return pre;
-        }
-
-        /**
-         * 返回最后一个小于val的右节点pre,val的值介于pre和pre.right之间
-         */
-        private TreeNode moveRight(TreeNode node, int val) {
-            TreeNode pre = node;
-            TreeNode p = node.right;
-            while (p != null && val > p.val) {
-                pre = p;
-                p = p.right;
-            }
-            return pre;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
