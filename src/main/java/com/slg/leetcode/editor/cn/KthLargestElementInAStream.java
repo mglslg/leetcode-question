645c96960da8018be4704d5348ec78dfa7eb57a0
索龙嘎 <suolongga@huli.com>
2021-03-31 16:45:13 +0800

    123

--- src/main/java/com/slg/leetcode/editor/cn/KthLargestElementInAStream.java
+++ src/main/java/com/slg/leetcode/editor/cn/KthLargestElementInAStream.java
@@ -1,5 +1,7 @@
 package com.slg.leetcode.editor.cn;
 
+import java.util.PriorityQueue;
+
 public class KthLargestElementInAStream {
 
     public static void main(String[] args) {
@@ -11,94 +13,58 @@ public class KthLargestElementInAStream {
         kth.add(10);
         kth.add(9);
         kth.add(4);
+
+        /*PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(1);
+        queue.add(5);
+        queue.add(2);
+        queue.add(19);
+        queue.add(20);
+        queue.add(15);
+        queue.add(6);
+        queue.add(8);
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.peek());
+            System.out.println(queue.poll());
+        }*/
+
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     /**
-     * 思路：构建一颗BST，第k大的元素即为排序数列中的第 length-k+1 个数
-     * 数左子树就是第m个数；数右子树就是第k大的。CntRoot=CntLeft+CntRight+1
-     * 其中length即为根节点的cnt值
-     *
-     * 但是这种解法目前解决不了重复数值的问题，可以考虑用一个map来记录重复值，然后每发现一次就令m+1或者k+1,之后继续找
-     * 因此这题还需要继续优化
+     * 思路：直接使用jdk的优先队列，其原理是一个小顶堆
+     * 构建一个大小为k的小顶堆，那么堆顶元素就是k个数里的最小值，也就是第k大的元素！
      */
     class KthLargest {
+        private PriorityQueue<Integer> pq;
         private int k;
-        private KNode kRoot;
 
         public KthLargest(int k, int[] nums) {
             this.k = k;
-
-            //这里构造bst，cnt应该是可以直接递归更新，看看怎么写
-            kRoot = new KNode(nums[0]);
-            for (int i = 1; i < nums.length; i++) {
-                kRoot = addNodeToBST(kRoot, nums[i]);
-            }
-        }
-
-        public int add(int val) {
-            kRoot = addNodeToBST(kRoot, val);
-            KNode target = findTarget(kRoot, this.k);
-            return target.val;
-        }
-
-        public KNode findTarget(KNode root, int k) {
-            //数列中的第m个值
-            int m = root.cnt - k + 1;
-
-            /**
-             * 左子树cnt+1代表第m个元素
-             * 右子树cnt就代表k+1就代表第k大
-             */
-            if ((root.cnt - subTreeCnt(root.right) == m)
-                    || (root.cnt - subTreeCnt(root.left) == k)) {
-                return root;
-            }else{
-                if (m <= subTreeCnt(root.left)) {
-                    return findTarget(root.left, k);
-                } else {
-                    return findTarget(root.right, k);
+            pq = new PriorityQueue(k);
+            for (int n : nums) {
+                if (pq.size() < k) {
+                    pq.offer(n);
+                }else{
+                    if (pq.peek() <= n) {
+                        pq.poll();
+                        pq.offer(n);//offer就相当于add
+                    }
                 }
             }
         }
 
-        //可以cover子树为空的情况，可不判断左右孩子是否为null而直接使用
-        private int subTreeCnt(KNode child) {
-            return child == null ? 0 : child.cnt;
-        }
-
-        //这棵树所有重复的值都在右侧
-        private KNode addNodeToBST(KNode root, int val) {
-            if (root == null) {
-                KNode leaf = new KNode(val);
-                leaf.cnt = 1;
-                return leaf;
+        public int add(int val) {
+            if (pq.size() < k) {
+                pq.offer(val);
+            }else{
+                if (val > pq.peek()) {
+                    pq.poll();
+                    pq.offer(val);
+                }
             }
-            if (val < root.val) {
-                //左子树一定是小于的
-                root.left = addNodeToBST(root.left, val);
-            } else {
-                //右子树有可能有等于的值
-                root.right = addNodeToBST(root.right, val);
-            }
-
-            //注意一定得记得这个1，加上自己！加上自己！加上自己！
-            root.cnt = 1 + (root.left == null ? 0 : root.left.cnt) + (root.right == null ? 0 : root.right.cnt);
-            return root;
-        }
-
-        private class KNode {
-            private int val;
-            private KNode left;
-            private KNode right;
-            private int cnt;
-
-            private KNode() {
-            }
-
-            private KNode(int _val) {
-                val = _val;
-            }
+            return pq.peek();
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
