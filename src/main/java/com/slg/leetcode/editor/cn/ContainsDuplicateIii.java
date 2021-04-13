c395b26f93817e270c86cdc08ac8ff17d389e780
索龙嘎 <suolongga@huli.com>
2021-04-13 18:14:06 +0800

    1

---
+++ src/main/java/com/slg/leetcode/editor/cn/ContainsDuplicateIii.java
@@ -0,0 +1,17 @@
+package com.slg.leetcode.editor.cn;
+
+public class ContainsDuplicateIii {
+    public static void main(String[] args) {
+        Solution solution = new ContainsDuplicateIii().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+            return false;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/com/slg/leetcode/editor/cn/ErChaSouSuoShuDeZuiJinGongGongZuXianLcof.java
+++ src/main/java/com/slg/leetcode/editor/cn/ErChaSouSuoShuDeZuiJinGongGongZuXianLcof.java
@@ -3,21 +3,30 @@ package com.slg.leetcode.editor.cn;
 import com.slg.leetcode.editor.cn.ds.TreeNode;
 
 public class ErChaSouSuoShuDeZuiJinGongGongZuXianLcof {
+
     public static void main(String[] args) {
         Solution solution = new ErChaSouSuoShuDeZuiJinGongGongZuXianLcof().new Solution();
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-
     /**
      * 找见一个节点，这个节点刚好比p大，比q小
-     * 如果root小于p并且小于q,那么就移动p和q到右子树；反之移动到左子树
-     * 当遍历到刚好root在p和q中间的时候，root就是最近公共节点！
+     * 如果root小于p并且小于q,说明pq在root的右侧，那么就移动到右子树；反之移动到左子树
      */
     class Solution {
-        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        private TreeNode result;
 
-            return null;
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) {
+                return null;
+            }
+            if (root.val < p.val && root.val < q.val) {
+                return lowestCommonAncestor(root.right, p, q);
+            } else if (root.val > p.val && root.val > q.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            } else {
+                return root;
+            }
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
