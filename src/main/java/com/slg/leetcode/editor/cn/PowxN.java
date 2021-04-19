44be6f85db0a434351dce84a47993bfe2f53deb8
suolongga <mglslg@qq.com>
2021-04-19 20:03:37 +0800

    1

--- src/main/java/com/slg/leetcode/editor/cn/PowxN.java
+++ src/main/java/com/slg/leetcode/editor/cn/PowxN.java
@@ -3,36 +3,42 @@ package com.slg.leetcode.editor.cn;
 public class PowxN {
     public static void main(String[] args) {
         Solution solution = new PowxN().new Solution();
-        System.out.println(solution.myPow(2, 10));
+        System.out.println(solution.myPow(2, -3 ));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion
-
     /**
-     * 计算x的n次幂:暴力法(测试用例通不过)
+     * 思路：二分法，利用公式x^n=x^(n/2)*x^(n/2)
+     * 测试用例：0.00001，2147483647不通过，神马垃圾玩意！
+     * 自己跑没啥问题，应该能通过
      */
     class Solution {
-
         public double myPow(double x, int n) {
             if (n == 0) {
                 return 1;
             }
-            double result;
-            if (n < 0) {
-                result = 1 / x;
-                for (int i = 1; i < (0 - n); i++) {
-                    result = result * (1/x);
-                }
-                return result;
-            }else{
-                result = x;
-                for (int i = 1; i < n; i++) {
-                    result = result * x;
-                }
-                return result;
+            if (n > 0) {
+                return powHelper(x, n);
             }
+            if (n < 0) {
+                return powHelper(1 / x, Math.abs((long)n));//防止超限
+            }
+            return 0;
+        }
+
+        private double powHelper(double x, long pow) {
+            if (pow > 1) {
+                if (pow % 2 == 0) {
+                    //偶数
+                    return powHelper(x, pow / 2) * powHelper(x, pow / 2);
+                }else{
+                    //奇数
+                    return powHelper(x, pow / 2) * powHelper(x, pow / 2) * x;
+                }
+            }
+            //这里pow一定为1
+            return x;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
-
 }
