84226c1a0b91e996fe690a46e8c51cc52c1fcf26
索龙嘎 <suolongga@huli.com>
2021-04-22 18:24:14 +0800

    Merge branch 'master' of github.com:mglslg/leetcode-question
    # Conflicts:
    #	src/main/java/com/slg/leetcode/editor/cn/XuLieHuaErChaShuLcof.java
    #	src/main/java/com/slg/leetcode/editor/cn/all.json

---
+++ src/main/java/com/slg/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+package com.slg.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            Map<Character, Integer> memo = new HashMap<>();
+
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
