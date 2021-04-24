76056e40232693e79a23b0bcb6fe84ad52b03d49
suolongga <mglslg@qq.com>
2021-04-24 22:36:34 +0800

    Merge branch 'master' of github.com:mglslg/leetcode-question
    # Conflicts:
    #	src/main/java/com/slg/leetcode/editor/cn/all.json

--- src/main/java/com/slg/leetcode/editor/cn/LruCache.java
+++ src/main/java/com/slg/leetcode/editor/cn/LruCache.java
@@ -1,7 +1,6 @@
 package com.slg.leetcode.editor.cn;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class LruCache {
@@ -13,13 +12,13 @@ public class LruCache {
      * 思路：用一个双向链表来置顶，用一个HashMap来存放缓存
      * 只有链表的前2个有资格在map里
      * 注意不管是put还是get都需要置顶
-     * 想要O(1)复杂度可以考虑map中存node而非直接存值
-     * 而在链表中删除node
+     * 如果想要O(1)复杂度的话只能自己设计链表，这样才能保证删除当前node时断开自己拼上左右链接
+     * 如果使用LinkedList那么无法访问ll内部的Node，它是private类型的
      */
     class LRUCache {
         int capacity = 0;
-        private LinkedList<Integer> keyList = new LinkedList<>();
-        private Map<Integer, Integer> cache = new HashMap<>();
+        private MyLinkedList keyList = new MyLinkedList();
+        private Map<Integer, Node> cache = new HashMap<>();
 
         public LRUCache(int capacity) {
             this.capacity = capacity;
@@ -28,27 +27,124 @@ public class LruCache {
         public int get(int key) {
             int rs = -1;
             if (cache.containsKey(key)) {
-                rs = cache.get(key);
-                //将key在链表中置顶
-                keyList.remove(keyList.indexOf(key));
-                keyList.addFirst(key);
+                rs = cache.get(key).value;
+                //置顶
+                keyList.remove(cache.get(key));
+                keyList.addFirst(cache.get(key));
             }
             return rs;
         }
 
         public void put(int key, int value) {
-            if (keyList.size() < capacity) {
-                if(!cache.containsKey(key)){
-                    cache.put(key, value);
-                    keyList.addFirst(key);
-                }else{
-                    //置顶
+            if (cache.containsKey(key)) {
+                //找到key对应的节点并设置新值
+                cache.get(key).value = value;
+                //将该节点置顶
+                keyList.remove(cache.get(key));
+                keyList.addFirst(cache.get(key));
+            } else {
+                if (keyList.getSize() < capacity) {
+                    //直接添加即可
+                    Node newNode = new Node(key, value);
+                    cache.put(key, newNode);
+                    keyList.addFirst(newNode);
+                } else {
+                    Node newNode = new Node(key, value);
+                    //加入顶部
+                    keyList.addFirst(newNode);
+                    //删除尾部
+                    Node lastNode = keyList.removeLast();
+                    //加入缓存
+                    cache.put(key, newNode);
+                    //从缓存中清除
+                    cache.remove(lastNode.key);
                 }
-            }else{
-                keyList.addFirst(key);
-                int last = keyList.removeLast();
-                cache.remove(last);
             }
+        }
+    }
+
+    /**
+     * 链表技巧：做任何操作之前，先把node.pre和node.next用一个新变量存起来
+     * 方便后面的一系列操作
+     */
+    class MyLinkedList {
+        private int size = 0;
+        private Node head;
+        private Node tail;
+
+        public int getSize() {
+            return size;
+        }
+
+        public void addFirst(Node node) {
+            size++;
+            if (head == null) {
+                head = node;
+                tail = node;
+            } else {
+                node.next = head;
+                head.pre = node;
+                head = node;
+            }
+        }
+
+        public Node removeLast() {
+            if (size > 0) {
+                size--;
+                Node removedTail = tail;
+                if (tail.pre != null) {
+                    Node tailPre = tail.pre;
+                    tailPre.next = null;
+                    tail = tailPre;
+                }else{
+                    //就剩最后一个节点了，删完全为空
+                    head = null;
+                    tail = null;
+                }
+                return removedTail;
+            }else{
+                return null;
+            }
+        }
+
+        public void remove(Node node) {
+            if (size > 0) {
+                size--;
+                Node nodePre = node.pre;
+                Node nodeNext = node.next;
+                if (nodePre != null) {
+                    if (nodeNext != null) {
+                        nodePre.next = nodeNext;
+                        nodeNext.pre = nodePre;
+                    }else{
+                        //要删除的就是tail
+                        nodePre.next = null;
+                        tail = nodePre;
+                    }
+                }else{
+                    if (nodeNext != null) {
+                        //要删除的是head节点
+                        nodeNext.pre = null;
+                        head = nodeNext;
+                    }else{
+                        //要删除的是链表中最后一个节点
+                        head = null;
+                        tail = null;
+                    }
+                }
+            }
+        }
+    }
+
+    class Node {
+        int key;
+        int value;
+        Node pre;
+        Node next;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
---
+++ src/main/java/com/slg/leetcode/editor/cn/MedianOfTwoSortedArrays.java
@@ -0,0 +1,17 @@
+package com.slg.leetcode.editor.cn;
+
+public class MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        Solution solution = new MedianOfTwoSortedArrays().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
